af4c2e0db0f3ee8b402cda7ad9461e1e03360f4a
root <admin@wplanet.ru>
2017-09-20 15:51:00 +0700

    russian or

--- messages/ru/messages.php
+++ messages/ru/messages.php
@@ -2,6 +2,6 @@
 
 return [
 	'You must fill at least {min} of the attributes {attributes}.' => 'Пожалуйста, заполните не менее {min} из следующих полей: {attributes}.',
-	' or ' => ' o ',
+	' or ' => ' или ',
 ];
 
